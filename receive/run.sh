221c0326be8e8b86ff0b101bc37708c534a989a4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-13 12:04:25 +0530

    changed

--- receive/run.sh
+++ receive/run.sh
@@ -1,2 +1,3 @@
-echo "receive db_host data...." >/data/db_host_value
-cat /data/db_host_value
+#echo "receive db_host data...." >/data/db_host_value
+#cat /data/db_host_value
+echo "hello" >/data/db_host_value
