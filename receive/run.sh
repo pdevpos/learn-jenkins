f87e2a2546251e38fb9f6d39809f183c1195fad5
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-13 14:20:11 +0530

    image changed

--- receive/run.sh
+++ receive/run.sh
@@ -1,4 +1,3 @@
 echo "Done!"
-
 source /data/db_host.txt
 echo $DB_HOST
--- send/run.sh
+++ send/run.sh
@@ -2,5 +2,6 @@ if [ -z "$DB_HOST" ]; then
  echo DB_HOST is missing
 else
   echo "export DB_HOST='$DB_HOST'">/data/db_host.txt
+
   export DB_HOST="mysql"
 fi
